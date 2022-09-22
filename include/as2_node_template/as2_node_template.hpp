3511ce870456ac26d68da20784fc87481a158f91
Miguel <miferco97@gmail.com>
2022-09-22 16:55:52 +0200

    Added .clang-format and style test added

--- include/as2_node_template/as2_node_template.hpp
+++ include/as2_node_template/as2_node_template.hpp
@@ -2,22 +2,20 @@
 #define AS2_NODE_TEMPLATE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
-#include "as2_core/node.hpp"
-#include "as2_core/names/topics.hpp"
-#include "as2_core/names/services.hpp"
 #include "as2_core/names/actions.hpp"
+#include "as2_core/names/services.hpp"
+#include "as2_core/names/topics.hpp"
+#include "as2_core/node.hpp"
 
-
-class As2NodeTemplate : public as2::Node
-{
-public:
+class As2NodeTemplate : public as2::Node {
+ public:
   As2NodeTemplate();
-  using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+  using CallbackReturn =
+      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-  CallbackReturn on_configure(const rclcpp_lifecycle::State & ) override;
-  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & ) override;
-  CallbackReturn on_shutdown(const rclcpp_lifecycle::State & ) override;
-
+  CallbackReturn on_configure(const rclcpp_lifecycle::State&) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State&) override;
+  CallbackReturn on_shutdown(const rclcpp_lifecycle::State&) override;
 };
 
-#endif //AS2_NODE_TEMPLATE_HPP_
+#endif  // AS2_NODE_TEMPLATE_HPP_
--- launch/as2_node_template_launch.py
+++ launch/as2_node_template_launch.py
@@ -6,11 +6,13 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value='drone0'),
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
         Node(
             package='as2_node_template',
             executable='as2_node_template_node',
             name='as2_node_template',
             namespace=LaunchConfiguration('drone_id'),
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
             output='screen',
             emulate_tty=True
         )
