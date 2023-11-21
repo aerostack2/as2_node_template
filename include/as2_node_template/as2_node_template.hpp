d86b7ff1779a3aaecad113682832c24071cb4c42
RPS98 <rafa.perez.s98@gmail.com>
2023-11-21 19:54:39 +0100

    Use ament_lint_auto

--- include/as2_node_template/as2_node_template.hpp
+++ include/as2_node_template/as2_node_template.hpp
@@ -1,25 +1,54 @@
-// "Copyright [year] <Copyright Owner>"
+// Copyright 2023 COPYRIGHT_HOLDER
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the COPYRIGHT_HOLDER nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef AS2_NODE_TEMPLATE_HPP_
-#define AS2_NODE_TEMPLATE_HPP_
+
+#ifndef AS2_NODE_TEMPLATE__AS2_NODE_TEMPLATE_HPP_
+#define AS2_NODE_TEMPLATE__AS2_NODE_TEMPLATE_HPP_
 
 #include <rclcpp/rclcpp.hpp>
 #include "as2_core/node.hpp"
 
-namespace as2_node_template {
+namespace as2_node_template
+{
 
-class As2NodeTemplate : public as2::Node {
+class As2NodeTemplate : public as2::Node
+{
   using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
 public:
   As2NodeTemplate();
   virtual ~As2NodeTemplate() = default;
 
-  CallbackReturn on_configure(const rclcpp_lifecycle::State&) override;
-  CallbackReturn on_deactivate(const rclcpp_lifecycle::State&) override;
-  CallbackReturn on_shutdown(const rclcpp_lifecycle::State&) override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_shutdown(const rclcpp_lifecycle::State &) override;
 };  // class As2NodeTemplate
 
 }  // namespace as2_node_template
 
-#endif  // AS2_NODE_TEMPLATE_HPP_
+#endif  // AS2_NODE_TEMPLATE__AS2_NODE_TEMPLATE_HPP_
--- src/as2_node_template.cpp
+++ src/as2_node_template.cpp
@@ -1,24 +1,58 @@
-// "Copyright [year] <Copyright Owner>"
+// Copyright 2023 COPYRIGHT_HOLDER
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the COPYRIGHT_HOLDER nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 
 #include "as2_node_template.hpp"
 
-using namespace as2_node_template;
+namespace as2_node_template
+{
 
-As2NodeTemplate::As2NodeTemplate() : as2::Node("as2_node_template") {}
+As2NodeTemplate::As2NodeTemplate()
+: as2::Node("as2_node_template") {}
 
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-CallbackReturn As2NodeTemplate::on_configure(const rclcpp_lifecycle::State& _state) {
+CallbackReturn As2NodeTemplate::on_configure(const rclcpp_lifecycle::State & _state)
+{
   // Set subscriptions, publishers, services, actions, etc. here.
   return CallbackReturn::SUCCESS;
-};
+}
 
-CallbackReturn As2NodeTemplate::on_deactivate(const rclcpp_lifecycle::State& _state) {
+CallbackReturn As2NodeTemplate::on_deactivate(const rclcpp_lifecycle::State & _state)
+{
   // Clean up subscriptions, publishers, services, actions, etc. here.
   return CallbackReturn::SUCCESS;
-};
+}
 
-CallbackReturn As2NodeTemplate::on_shutdown(const rclcpp_lifecycle::State& _state) {
+CallbackReturn As2NodeTemplate::on_shutdown(const rclcpp_lifecycle::State & _state)
+{
   // Clean other resources here.
   return CallbackReturn::SUCCESS;
-};
+}
+
+}  // namespace as2_node_template
